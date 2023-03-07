a93da269a2606a02d16a2fbd1dbedcd995488d23
Sai Vishwa <114056075+SaiVishwa021@users.noreply.github.com>
2023-03-07 06:29:27 +0530

    Update and rename day1 to day1.java

--- day1.java
+++ day1.java
@@ -1,6 +1,6 @@
-#task1: In this challenge, you must read  integers from stdin and then print them to stdout. 
-#Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
-#soution as follows
+// task1: In this challenge, you must read  integers from stdin and then print them to stdout. 
+// Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+// soution as follows
 
 
 import java.util.*;
